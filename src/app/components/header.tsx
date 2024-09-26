fef4aad08d339161021235c9c328d6cb708b13d8
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-09-26 15:10:37 -0300

    first commit

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="bg-black flex justify-center items-center py-5 w-full h-48 absolute top-0 left-1">
       <Image
-        src="/Logo.svg"
+        src="/logo.svg"
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-32 h-32"
         width={480}
         height={480}
